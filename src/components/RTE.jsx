76957017daa966119477e36a280f7be590dc7ab9
SurajSingh <er.suraj.singh0007@gmail.com>
2024-02-20 22:35:02 +0530

    inmaking

---
+++ src/components/RTE.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Editor } from '@tinymce/tinymce-react';
+import { Controller } from 'react-hook-form';
+
+function RTE() {
+  return (
+    <div>RTE</div>
+  )
+}
+
+export default RTE
