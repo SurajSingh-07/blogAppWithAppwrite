0846002cf5dfd5d14095f58526ff3799e6ded73d
SurajSingh <er.suraj.singh0007@gmail.com>
2024-02-22 23:02:23 +0530

    Almost Completed

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -8,7 +8,7 @@ export class AuthService {
   constructor() {
     this.client
       .setEndpoint(conf.appwriteUrl)
-      .setProject(conf.appwriteProjectID);
+      .setProject(conf.appwriteProjectId);
     this.account = new Account(this.client);
   }
 
